c64249b0cd85c4e5780349a76dc0eda217ac34f3
Bradley Moore <bradleym007@gmail.com>
2017-08-04 16:06:37 -0700

    Added more functions labels

--- wp-zendesk-api.php
+++ wp-zendesk-api.php
@@ -164,5 +164,260 @@ if( ! class_exists( 'WpZendeskAPI' ) ){
 
     /* Ticket import */
 
+    public function ticket_import(){
+
+    }
+
+    public function bulk_ticket_import(){
+
+    }
+
+    /* Requests */
+
+    public function list_requests(){
+
+    }
+
+    public function search_requests(){
+
+    }
+
+    public function show_request(){
+
+    }
+
+    public function create_request(){
+
+    }
+
+    public function update_request(){
+
+    }
+
+    public function set_collaborators_request(){
+
+    }
+
+    public function add_collaborators_request(){
+
+    }
+
+    public function list_comments_request(){
+
+    }
+
+    public function get_comment_request(){
+
+    }
+
+    /* Attachments */
+
+    public function show_attachment(){
+
+    }
+
+    public function delete_attachment(){
+
+    }
+
+    public function upload_files(){
+
+    }
+
+    public function delete_upload(){
+
+    }
+
+    public function redact_comment_attachment(){
+
+    }
+
+    /* Satisfaction Ratings */
+
+    public function list_satisfaction_ratings(){
+
+    }
+
+    public function show_satisfaction_rating(){
+
+    }
+
+    public function create_satisfaction_rating(){
+
+    }
+
+    /* Satisfaction Reasons */
+
+    public function list_reasons_for_satisfaction_rating(){
+
+    }
+
+    public function show_reasons_for_satisfaction_rating(){
+
+    }
+
+    /* Suspended Tickets */
+
+    public function list_suspended_tickets(){
+
+    }
+
+    public function show_suspended_tickets(){
+
+    }
+
+    public function recover_suspended_ticket(){
+
+    }
+
+    public function recover_suspended_tickets(){
+
+    }
+
+    public function delete_suspended_ticket(){
+
+    }
+
+    public function delete_suspended_tickets(){
+
+    }
+
+    /* Ticket Audits */
+
+    public function list_audits_for_ticket(){
+
+    }
+
+    public function show_audit(){
+
+    }
+
+    public function change_comment_to_private(){
+
+    }
+
+    public function get_audit_events(){
+
+    }
+
+    public function the_via_object(){
+
+    }
+
+    /* Ticket Comments */
+
+    public function create_ticket_comment(){
+
+    }
+
+    public function list_comments(){
+
+    }
+
+    public function redact_string_in_comment(){
+
+    }
+
+    public function make_comment_private(){
+
+    }
+
+    /* Ticket skips */
+
+    public function record_skip_for_user(){
+
+    }
+
+    public function list_skips_for_account(){
+
+    }
+
+    /* Ticket metrics */
+
+    /* Ticket metric events */
+
+    /* Users */
+
+    /* User identities */
+
+    /* Custom agent roles */
+
+    /* End users */
+
+    /* Groups */
+
+    /* Group memberships */
+
+    /* Sessions */
+
+    /* Organizations */
+
+    /* Organization Subscriptions */
+
+    /* Organization Memberships */
+
+    /* Automations */
+
+    /* Macros */
+
+    /* SLA Policies */
+
+    /* Targets */
+
+    /* Triggers */
+
+    /* Views */
+
+    /* Account Settings */
+
+    /* Audit Logs */
+
+    /* Brands */
+
+    /* Dynamic content */
+
+    /* Locales */
+
+    /* Organization Fields */
+
+    /* Schedules */
+
+    /* Sharing agreements */
+
+    /* Support addresses */
+
+    /* Ticket forms */
+
+    /* Ticket fields */
+
+    /* User fields */
+
+    /* Apps */
+
+    /* App installation locations */
+
+    /* App locations */
+
+    /* OAuth clients */
+
+    /* OAuth tokens */
+
+    /* Authorized global clients */
+
+    /* Activity stream */
+
+    /* Bookmarks */
+
+    /* Push notification devices */
+
+    /* Resource collections */
+
+    /* Tags */
+
+    /* Channel framework */
+
+    /* Twitter channel */
+    
+
   }
 }
